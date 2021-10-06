3f8e678416abdbdec1c90acb8a7ac2aa0c713ccb
carol <carolynw898@gmail.com>
2021-10-06 11:24:58 -0400

    Removed hardcoded N from ENac.py and NCC.py, now using cell.total instead.

--- Newton.py
+++ Newton.py
@@ -301,7 +301,7 @@ def newton_rat(func,x,k,cell):
         x-= delta
         f = np.matrix(fun(x,k))
         iter+=1
-        #print(iter, np.linalg.norm(f))
+        print(iter, np.linalg.norm(f))
         TOLpcn = np.max(delta/x)
     return x
     
@@ -588,7 +588,7 @@ def newton_human(func,x,k,cell):
         x -= delta
         f = np.matrix(fun(x,k))
         iter += 1
-        #print(iter, np.linalg.norm(f))
+        print(iter, np.linalg.norm(f))
         TOLpcn = np.max(delta / x)
     return x
         
